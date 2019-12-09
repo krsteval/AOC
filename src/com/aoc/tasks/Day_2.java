046631f39ca98cd1cc206c7e9b5231dc1d4e8f32
Krste Moskov <krste.moskov@sourcico.com>
2019-12-09 02:40:14 +0100

    update 2

--- src/com/aoc/tasks/Day_2.java
+++ src/com/aoc/tasks/Day_2.java
@@ -12,32 +12,60 @@ import com.aoc.helpers.Common;
 public class Day_2 {
 
 	List<Integer> inputNumbers;
+	List<Integer> DInputNumbers = new ArrayList<Integer>();
 
 	public Day_2(List<Integer> inputNumbers) {
 		this.inputNumbers = inputNumbers;
-		this.inputNumbers.add(1,12);
-		this.inputNumbers.add(2,2);
+		this.DInputNumbers.addAll(inputNumbers);
 		
 	}
+	
 
-	public void calculateTask1() {
-		for (int i = 0; i < 99; i+=4) {
+	public void calculateTask1(int ...params) {
+	
+		inputNumbers.set(1, params.length==0 ? 12 : params[0]);
+		inputNumbers.set(2, params.length==0 ? 2  : params[1]);
+
+		for (int i = 0; i < inputNumbers.size(); i+=4) {
 			Integer storePos = inputNumbers.get(i+3);
-			if(inputNumbers.get(i)==1) {  
-				inputNumbers.add(storePos, calculateOpcode(inputNumbers.get(i+1), inputNumbers.get(i+2), '+'));
-			
-			}
-			else if(inputNumbers.get(i)==2) {
-				inputNumbers.add(calculateOpcode(inputNumbers.get(i+1), inputNumbers.get(i+2), '*'));
+			if(storePos>=inputNumbers.size())
+				return;
+			switch (inputNumbers.get(i)) {
+			case 1:
+				inputNumbers.set(storePos, inputNumbers.get(inputNumbers.get(i+1)) + inputNumbers.get(inputNumbers.get(i+2)));
+				break;
+			case 2:
+				inputNumbers.set(storePos, inputNumbers.get(inputNumbers.get(i+1)) * inputNumbers.get(inputNumbers.get(i+2)));
+				break;
+			case 99:
+
+//				System.out.println("Zad 1: " + inputNumbers.toString());
 				
+				System.out.println("---99--->" + inputNumbers.get(0));
+				return;
 			}
 		}
 
-		System.out.println("Zad 1: " + inputNumbers.toString());
+	}
+	
+	public void calculateTask2() {
+		for (int i = 0; i < 100; i++) {
+			for (int j = 0; j < 100; j++) {
+				calculateTask1(i, j);
+				if(inputNumbers.get(0) == 19690720)
+				{
+					System.out.println("ZAD 2");
+					
+					System.out.println(100 * i + j);
+				}
+				inputNumbers.clear();
+				inputNumbers.addAll(DInputNumbers);
+			}
+		}
 	}
 
 	public Integer calculateOpcode(int fistItemPosition, int secondAfterNextPossition, char operacija) {
-		return operacija=='+' ? (inputNumbers.get(fistItemPosition) + inputNumbers.get(secondAfterNextPossition)): (inputNumbers.get(fistItemPosition) * inputNumbers.get(secondAfterNextPossition));
+		return operacija=='+' ? (inputNumbers.get(fistItemPosition) + inputNumbers.get(secondAfterNextPossition)) : (inputNumbers.get(fistItemPosition) * inputNumbers.get(secondAfterNextPossition));
 		
 	}
 	
--- src/com/aoc/tasks/Main.java
+++ src/com/aoc/tasks/Main.java
@@ -8,12 +8,14 @@ public class Main {
 		// TODO Auto-generated method stub
 
 		try {
-			Day_1 zadaca_1 = new Day_1(Common.getInstance().readInput(1));
-			zadaca_1.calculateTask1();
-			zadaca_1.calculateTask2();
+//			Day_1 zadaca_1 = new Day_1(Common.getInstance().readInput(1));
+//			zadaca_1.calculateTask1();
+//			zadaca_1.calculateTask2();
 
 			Day_2 zadaca_2 = new Day_2(Common.getInstance().readInput(2));
 			zadaca_2.calculateTask1();
+			zadaca_2 = new Day_2(Common.getInstance().readInput(2));
+			zadaca_2.calculateTask2();
 			
 		}catch (Exception e) {
 			// TODO: handle exception
